d3c0c8dde1bb0d6e42259256053bf380d47b79e1
Georgie <gbenevento@cifpfbmoll.eu>
2021-03-05 15:45:04 +0100

    Created @BeforeEach in PuntuacioTest

--- test/PuntuacioTest.java
+++ test/PuntuacioTest.java
@@ -1,7 +1,15 @@
 package test;
 
+import com.georgie.penjat.Puntuacio;
+import org.junit.jupiter.api.BeforeEach;
+
 public class PuntuacioTest {
 
+    private Puntuacio puntuacio;
 
+    @BeforeEach
+    void reiniciar() {
+        this.puntuacio = new Puntuacio();
+    }
 
 }
